b288c7a1db0dbe42246c3a1617b7edf502a11b24
Ephantus karanja <ephantusslogan@gmail.com>
2018-08-02 19:25:21 +0300

    create posts model

--- my_blog/models.py
+++ my_blog/models.py
@@ -1,3 +1,7 @@
 from django.db import models
-
+from datetime import datetime
 # Create your models here.
+class Posts(models.Model):
+    title=models.CharField(max_length=100, blank=True)
+    body=models.TextField(blank=True)
+    created_at=models.DateField(default=datetime.now, blank=True)
