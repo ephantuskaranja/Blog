758f49a2957c259aa5e3c761a514e8962fc01ebc
Ephantus karanja <ephantusslogan@gmail.com>
2018-08-06 19:30:24 +0300

    show post details in details.html

--- my_blog/views.py
+++ my_blog/views.py
@@ -1,11 +1,20 @@
 from django.shortcuts import render
-
+from .models import Posts
 # Create your views here.
 def index(request):
-    
+    posts= Posts.objects.all()
     context={
         "title":'My latest Post',
+        "posts":posts
 
     }
     
     return render(request, 'index.html', context)
+
+def details(request, id):
+    post=Posts.objects.get(id=id)
+    context = {
+        "post":post
+    }
+
+    return render(request, 'details.html', context)
