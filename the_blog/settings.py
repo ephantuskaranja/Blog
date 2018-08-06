06fd2924f5ace07de90df891df52c60e3d6885ed
Ephantus karanja <ephantusslogan@gmail.com>
2018-08-06 14:57:49 +0300

    install postgres and configure db

--- the_blog/settings.py
+++ the_blog/settings.py
@@ -76,8 +76,10 @@ WSGI_APPLICATION = 'the_blog.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': 'blog',
+        'USER': 'ephantus',
+        'PASSWORD': 'switcher12',
     }
 }
 
